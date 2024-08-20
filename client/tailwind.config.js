875f00c6351084e7c6ed13278604ec14d2e3804f
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2024-08-20 23:02:50 +0530

    fix: tailwind config add

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "blinks-blue": "#1E90FF",
+        "blinks-yellow": "#FFD700",
+        "midnight-purple": "#6A0D91",
+        "sunset-orange": "#FF4500",
+        "charcoal-gray": "#333333",
+        "light-gray": "#F5F5F5",
+        "seafoam-green": "#2E8B57",
+        "steel-blue": "#4682B4",
+      },
+    },
+  },
+  plugins: [],
+};
